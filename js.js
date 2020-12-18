daa6727b633ddcba7c8427a5a8e218c5e3726b3a
Aika Darling <zh.aikerim@gmail.com>
2020-12-17 21:38:48 -0600

    added lat and lon

---
+++ js.js
@@ -0,0 +1,38 @@
+var inputValue = document.querySelector(".input-value")
+
+  var apiKey = '1b4f8b955a88a2ead047909c85a25270'
+ 
+
+
+  $('#button').on('click',function(){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=" + apiKey + '&units=imperial'
+
+    $.ajax({
+    url: queryURL,
+   method: 'GET',
+
+  }).then(function(weatherData){
+    console.log(weatherData)
+   
+    $('.city').text('City: ' + weatherData.name)
+    $('.wind').text('Wind Speed: ' + weatherData.wind.speed)
+    $('.humidity').text('Humidity: ' + weatherData.main.humidity)
+    $('.temp').text('Temp: ' + weatherData.main.temp + " C")
+    $('.lat').text('Lat: '+ weatherData.coord.lat)
+    $('.lon').text('Lon: '+ weatherData.coord.lon)
+    
+   var queryTwoURL = "http://api.openweathermap.org/data/2.5/uvi?lat="+ weatherData.coord.lat+"&lon=" + weatherData.coord.lon + "&appid=" + apiKey
+
+    $.ajax({
+    url: queryTwoURL,
+    method: 'GET',
+
+    }).then(function(Data){
+      console.log(data)
+    })
+  })
+})
+  
+
+
+
